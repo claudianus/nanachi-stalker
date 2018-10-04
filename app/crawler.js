24e3442cea61fa41eeb88f84315060cc7deb0a7f
Claudianus Mamertus <claudianus@engineer.com>
2018-10-04 21:09:16 +0900

    Updated
    Now it uses google as a search engine to get images and some useful properties has been added to .env.example file

--- app/crawler.js
+++ app/crawler.js
@@ -1,7 +1,7 @@
 //크롤러
-//겔부루에서 나나치 사진 1분마다 따옴
-const Kaori = require('kaori')
-const kaori = new Kaori();
+//구글에서 나나치 사진 1분마다 따옴
+const google = require('google-parser');
+const Env = use('Env')
 
 global.images = []
 
@@ -11,12 +11,8 @@ setInterval(async () => {
 }, 60000)
 
 async function crawl () {
-    images = await kaori.search('gelbooru', {
-        tags: ['nanachi_(made_in_abyss) rating:safe'],
-        limit: 200,
-        random: false
-    })
-    console.log('crawled!')
+    images = await google.img(Env.get('SEARCH_QUERY', 'nanachi filetype:gif'))
+    console.log('Image Updated!')
 }
 
 console.log('crawler started!')
